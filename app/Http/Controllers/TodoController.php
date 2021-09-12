309c0b5fe649952597a5416a9e57ad07f83e33ca
Ayako1911 <85421490+Ayako1911@users.noreply.github.com>
2021-09-12 21:44:52 +0900

    Update TodoController.php
    createメソッド（addメソッドも）を追記

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -9,7 +9,24 @@ class TodoController extends Controller
 {
     public function index()
     {
-        $todo = ToDo::all();
-        return view('todo.index')->with('todo', $todo);
+        $items = ToDo::all();
+        return view('index',['items' => $items]);
     }
+    //↓データを追加するにあたってaddアクションが必要なのかどうかが分かりません。
+    //public function add()
+    //{
+    //    return view('/todo/create');
+    //}
+    
+     public function create(StoreRequest $request)
+     //↑StoreRequestにバリデーションを設定しているため、Requestではなく、StoreRequestにしています。
+     {
+         //↓新規のToDoモデルを作成する
+         $item = new ToDo;
+         //↓contentをToDoモデルに設定する
+         $item->content = $request->content;
+         //↓DBにデータを登録する
+         $item->save();
+         //↓リダイレクトで/に飛ばす
+         return redirect('/');
 }
